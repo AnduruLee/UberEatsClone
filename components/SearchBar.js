04835149f23c56144523131e8b83d56fdf79a8a6
AndrewLee <drewlee213@gmail.com>
2022-06-20 11:31:02 -0700

    HeaderTabs Enter

---
+++ components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';  
+
+
+export default function SearchBar() {
+  return (
+    <View>
+      <Text>SearchBar</Text>
+    </View>
+  )
+}
