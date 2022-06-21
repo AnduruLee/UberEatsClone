9a66b88703bddec5bfdfede3db7a5dc28eaa7ffc
AndrewLee <drewlee213@gmail.com>
2022-06-20 18:52:01 -0700

    RestaurantItems

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, SafeAreaView, ScrollView } from 'react-native';
 import Categories from '../components/Categories';
 import HeaderTabs from '../components/HeaderTabs';
-import RestaurantItem from '../components/RestaurantItem';
+import RestaurantItems, { localRestaurants } from '../components/RestaurantItems';
 import SearchBar from '../components/SearchBar';
 
 export default function Home() {
+    const [restaurantData, setRestaurantData] = useState(localRestaurants)
+
+    const getRestaurantsFromYelp = () => {
+        const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=Hollywood`;
+    
+
+    const apiOptions = {
+        headers: {
+            Authorization: `Bearer ${YELP_API_KEY}`,
+        },
+    };
+
+    return fetch(yelpUrl, apiOptions)
+        .then((res) => res.json())
+        .then(json => setRestaurantData(json.businesses));
+};
+
+useEffect(() =>{
+    getRestaurantsFromYelp();
+}, []);
+
+
   return (
     <SafeAreaView style={{ backgroundColor: '#eee', flex: 1 }}>
         <View style={{ backgroundColor: 'white', padding: 15 }}>
@@ -14,7 +36,7 @@ export default function Home() {
         </View>
         <ScrollView showsVerticalScrollIndicator={false}>
             <Categories />
-            <RestaurantItem />
+            <RestaurantItems restaurantData={restaurantData} />
         </ScrollView>
     </SafeAreaView>
   );
