46cec049295f7e4ae286c6415e5fea59fb61990a
AndrewLee <drewlee213@gmail.com>
2022-06-20 23:18:42 -0700

    YelpAPI

--- screens/Home.js
+++ screens/Home.js
@@ -5,28 +5,31 @@ import HeaderTabs from '../components/HeaderTabs';
 import RestaurantItems, { localRestaurants } from '../components/RestaurantItems';
 import SearchBar from '../components/SearchBar';
 
+const YELP_API_KEY = 
+    'HfoJgwWoPXGhQDBqr8LFO_6VDm56S7sW48LlHyghr2dHeTvQvHTEjgyabpsGbxem719q886-pu_9zi1XXoc2M3BREXqx57oZLM3k-GP9CHd_3QTN3d4WQb9TJt6wYnYx';
+
 export default function Home() {
     const [restaurantData, setRestaurantData] = useState(localRestaurants)
 
     const getRestaurantsFromYelp = () => {
         const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=Hollywood`;
-    
 
-    const apiOptions = {
-        headers: {
-            Authorization: `Bearer ${YELP_API_KEY}`,
-        },
-    };
 
-    return fetch(yelpUrl, apiOptions)
-        .then((res) => res.json())
-        .then(json => setRestaurantData(json.businesses));
+        const apiOptions = {
+            headers: {
+                Authorization: `Bearer ${YELP_API_KEY}`,
+            },
+        };
+
+        return fetch(yelpUrl, apiOptions)
+            .then((res) => res.json())
+            .then(json => setRestaurantData(json.businesses));
 };
 
 useEffect(() =>{
-    getRestaurantsFromYelp();
-}, []);
-
+        getRestaurantsFromYelp();
+    }, []);
+    
 
   return (
     <SafeAreaView style={{ backgroundColor: '#eee', flex: 1 }}>
@@ -40,4 +43,4 @@ useEffect(() =>{
         </ScrollView>
     </SafeAreaView>
   );
-}
+};  
